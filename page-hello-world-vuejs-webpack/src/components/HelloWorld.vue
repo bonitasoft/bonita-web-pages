757a0620be7a2be90f421c37248d20e181899b4f
JonathanPiquard <piquard.jonathan@protonmail.com>
2018-02-21 12:28:34 +0100

    change hello world template by vuejs to a bonita one

---
+++ page-hello-world-vuejs-webpack/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-bind:key="process.name" v-for="process in processes">
+        {{ process.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: "Hello world with Vuejs",
+      processes: [
+        { name: 'process1' },
+        { name: 'process2' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
