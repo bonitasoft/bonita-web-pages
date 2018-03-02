f4e3eccca079d217126a3d365f8d03fa03b6d3d4
benjaminparisel <benjamin.parisel@bonitasoft.com>
2018-03-02 14:59:28 +0100

    remove css file and use scss

--- page-hello-world-react/src/App.js
+++ page-hello-world-react/src/App.js
@@ -1,39 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import fetchProcesses from '@bonita/api/src/fetchProcesses.js';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            processes: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        processes: []
+    };
+  }
 
-    componentDidMount() {
-        fetchProcesses().then((processes) => this.setState({processes}));
-    }
+  componentDidMount() {
+    fetchProcesses().then((processes) => this.setState({ processes }));
+  }
 
-    render() {
-        const {processes} = this.state;
+  render() {
+    const { processes } = this.state;
 
-        return (
-            <div className="container">
-                <h1>Hello world with React</h1>
-                <div className="flash">
-                    <button type="submit" className="btn btn-sm primary flash-action">Complete action</button>
-                    Flash message with action here.
-                </div>
-                <button className="btn btn-primary" type="button">Button</button>
-                <a className="btn" href="https://primer.github.io/" role="button">Primer showroom</a>
-                <ul>
-                    {
-                        processes.map((process) => <li>{process.displayName}</li>)
-                    }
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div className="container">
+        <h1>Hello world with React</h1>
+        <div className="flash">
+            <button type="submit" className="btn btn-sm primary flash-action">Complete action</button>
+            Flash message with action here.
+        </div>
+        <button className="btn btn-primary" type="button">Button</button>
+        <a className="btn" href="https://primer.github.io/" role="button">Primer showroom</a>
+        <ul>
+            {
+                processes.map((process) => <li>{process.displayName}</li>)
+            }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
