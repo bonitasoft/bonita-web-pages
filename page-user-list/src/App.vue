65f8f4cd0f16fcd634320574b97b980f11bc4460
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-04-12 10:08:34 +0200

    clean init page

--- page-user-list/src/App.vue
+++ page-user-list/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -18,11 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
