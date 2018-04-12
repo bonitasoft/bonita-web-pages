c78d1186a35dbb89dc3422fcb24d1a2fc44fbe53
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-04-12 10:11:27 +0200

    feat(user-list-page) init user list page with vue-cli (#28)

---
+++ page-user-list/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
