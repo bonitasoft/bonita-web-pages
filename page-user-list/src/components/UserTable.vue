3cad55541a70e46e69792ed660d95192a2298232
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-04-12 15:05:29 +0200

    fix lint

---
+++ page-user-list/src/components/UserTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     <b-table striped hover :items="users" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserTable",
+  props: {
+    users: Array
+  },
+  data() {
+    return {
+      fields: [
+        "userName",
+        "lastname",
+        "firstname",
+        "job_title",
+        "last_connection"
+      ]
+    };
+  }
+};
+</script>
