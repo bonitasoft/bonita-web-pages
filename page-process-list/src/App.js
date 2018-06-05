cf25d96362596fcf0ded3d21e8742a83029db964
julienmege <julien.mege@bonitasoft.com>
2018-06-05 11:46:51 +0200

    Set ifram target without parsing URL

--- page-process-list/src/App.js
+++ page-process-list/src/App.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Main } from './pages';
-import { HashRouter , Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Instantiation from './pages/Main/components/Instantiation/Instantiation';
 
 class App extends Component {
-    render() {
-        return (
-            <HashRouter>
-                <div>
-                    <Route exact path="/" component={Main} />
-                    <Route
-                        path="/instantiation/:processName/:processVersion"
-                        component={Instantiation}
-                    />
-                </div>
-            </HashRouter>
-        );
-    }
+  render() {
+    return (
+      <HashRouter>
+        <div className="ProcessList">
+          <Route exact path="/" component={Main} />
+          <Route
+            path="/instantiation/:processName/:processVersion"
+            component={Instantiation}
+          />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
--- page-process-list/src/pages/Main/components/Instantiation/Instantiation.js
+++ page-process-list/src/pages/Main/components/Instantiation/Instantiation.js
@@ -3,17 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Instantiation.css';
 
 class Instantiation extends Component {
-
-    getInstantiationUrl(){
+  getInstantiationUrl() {
     const processName = this.props.match.params.processName;
     const processVersion = this.props.match.params.processVersion;
-
-    let url = `/portal/resource/process/${processName}/${processVersion}/content/`;
+    let url = `../../../../process/${processName}/${processVersion}/content/`;
+    console.log(url);
     return url;
-    }
+  }
+
+  getUrlContext() {
+    const pathName = this.getWindowLocationHref();
+    const portalIndex = pathName.indexOf('/portal');
+    return pathName.slice(0, portalIndex);
+  }
+
+  getWindowLocationHref() {
+    return window.location.href;
+  }
 
   render() {
-        const url = this.getInstantiationUrl();
+    const url = this.getInstantiationUrl();
     return (
       <iframe
         className="Instantiation container border"
