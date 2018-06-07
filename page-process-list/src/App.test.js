93b6b228410e4d5a11a2d17dda6d13b358ac4060
benjaminparisel <benjamin.parisel@bonitasoft.com>
2018-06-07 16:03:07 +0200

    test(app): fix test after hashrouter integration
    Covers [BS-18415](https://bonitasoft.atlassian.net/browse/BS-18415)

--- page-process-list/src/App.test.js
+++ page-process-list/src/App.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import App from './App';
-import Main from './pages/Main';
 import { shallow } from 'enzyme';
+import { HashRouter } from "react-router-dom";
 
 describe('App', () => {
   it('should display main page if session is loaded', () => {
     const session = { user_id: 1 };
     const wrapper = shallow(<App session={session} />);
 
-    expect(wrapper.find(Main).exists()).toBe(true);
+    expect(wrapper.find(HashRouter).exists()).toBe(true);
   });
 
   it('should display loader if session is not loaded', () => {
