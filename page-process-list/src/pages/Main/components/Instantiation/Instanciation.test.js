8bed186a9346f1ca3bc9041219baf46967f6040a
julienmege <julien.mege@bonitasoft.com>
2018-06-11 18:31:18 +0200

    test EventListener

--- page-process-list/src/pages/Main/components/Instantiation/Instanciation.test.js
+++ page-process-list/src/pages/Main/components/Instantiation/Instanciation.test.js
@@ -1,53 +1,46 @@
 import React from "react";
 
 import Instantiation from "./Instantiation";
-import { shallow } from "enzyme";
+import {shallow} from "enzyme";
 
 describe("Instantiation page", () => {
-  const props = {
-    match: {
-      params: {
-        processName: "My process name",
-        processVersion: "1.0"
-      }
-    },
-    location: {
-      search: "?id=1&autoInstantiate=false"
-    }
-  };
-
-  it("should display instantiation form", () => {
-    const wrapper = shallow(<Instantiation {...props} />);
-
-    expect(wrapper.find("iframe").prop("src")).toEqual(
-      "../../../../process/My process name/1.0/content/?id=1&autoInstantiate=false"
-    );
-  });
-
-  it("should add an event listenenr when component is created", () => {
-    window.addEventListener = jest.fn();
-    const wrapper = shallow(<Instantiation {...props} />);
-
-    expect(window.addEventListener).toHaveBeenCalled();
-  });
-
-  it("should redirect to home page after submit", () => {
-    const testProps = {
-      ...{
-        history: { push: jest.fn() }
-      },
-      ...props
-    };
-    const wrapper = shallow(<Instantiation {...testProps} />);
-    let event = {
-      data: {
-        action: "Start process",
-        message: "success"
-      }
+    const props = {
+        match: {
+            params: {
+                processName: "My process name",
+                processVersion: "1.0"
+            }
+        },
+        location: {
+            search: "?id=1&autoInstantiate=false"
+        }
     };
 
-    wrapper.instance().onFormSubmited(event);
+    it("should display instantiation form", () => {
+        const wrapper = shallow(<Instantiation {...props} />);
 
-    expect(testProps.history.push).toHaveBeenCalledWith("/");
-  });
+        expect(wrapper.find("iframe").prop("src")).toEqual(
+            "../../../../process/My process name/1.0/content/?id=1&autoInstantiate=false"
+        );
+    });
+
+    it('should update history on success submit message.', async () => {
+        const testProps = {
+            ...props,
+            history: {push: jest.fn()}
+        };
+        const wrapper = shallow(<Instantiation {...testProps} />);
+
+        // Simulate a message being sent
+        var message = {
+                    action: 'Start process',
+                    message: 'success'
+                };
+
+        window.postMessage(message, '*');
+        //New to add this Promise to allows message to be read
+        await new Promise(resolve => setTimeout(resolve, 0));
+        expect(testProps.history.push).toHaveBeenCalledWith("/");
+    });
+
 });
--- page-process-list/src/pages/Main/components/Instantiation/Instantiation.js
+++ page-process-list/src/pages/Main/components/Instantiation/Instantiation.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Instantiation.css';
 
 export default class Instantiation extends Component {
 
-  constructor(props) {
-      super(props);
-      this.onFormSubmited = this.onFormSubmited.bind(this);
-      window.addEventListener("message", this.onFormSubmited, false);
-  }
+    constructor(props) {
+        super(props);
+        this.onFormSubmited = this.onFormSubmited.bind(this);
+        this.messageListener = window.addEventListener('message', this.onFormSubmited, false);
+    }
 
-  onFormSubmited(message) {
-      const messageData =  message.data;
-      var jsonMessage = typeof messageData === 'string' ? JSON.parse(messageData) : messageData;
+    componentWillUnmount() {
+        window.removeEventListener('message', this.messageListener);
+    }
+
+    onFormSubmited(message) {
+        const messageData = message.data;
+        var jsonMessage = typeof messageData === 'string' ? JSON.parse(messageData) : messageData;
         if (jsonMessage.action === 'Start process' && jsonMessage.message === 'success') {
-                this.props.history.push('/');
+            this.props.history.push('/');
         }
     };
 
-  render() {
-    const { processName, processVersion } = this.props.match.params;
+    render() {
+        const {processName, processVersion} = this.props.match.params;
 
-    return (
-      <iframe
-        className="Instantiation container border"
-        src={`../../../../process/${processName}/${processVersion}/content/${this.props.location.search}`}
-        title="Instantiation"
-      />
-    );
-  }
+        return (
+            <iframe
+                className="Instantiation container border"
+                src={`../../../../process/${processName}/${processVersion}/content/${this.props.location.search}`}
+                title="Instantiation"
+            />
+        );
+    }
 }
