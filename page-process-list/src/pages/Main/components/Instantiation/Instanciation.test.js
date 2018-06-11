55708b61d10d928c6d98616c2afe6517c499d7bb
benjaminparisel <benjamin.parisel@bonitasoft.com>
2018-06-11 17:02:58 +0200

    test(instantiation): write unit test

--- page-process-list/src/pages/Main/components/Instantiation/Instanciation.test.js
+++ page-process-list/src/pages/Main/components/Instantiation/Instanciation.test.js
@@ -1,47 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import Instantiation from './Instantiation';
-import {shallow} from 'enzyme';
+import Instantiation from "./Instantiation";
+import { shallow } from "enzyme";
 
-describe('Instantiation page', () => {
+describe("Instantiation page", () => {
+  const props = {
+    match: {
+      params: {
+        processName: "My process name",
+        processVersion: "1.0"
+      }
+    },
+    location: {
+      search: "?id=1&autoInstantiate=false"
+    }
+  };
 
-    const props = {
-        match: {
-            params: {
-                processName: 'My process name',
-                processVersion: '1.0'
-            }
-        },
-        location: {
-            search: '?id=1&autoInstantiate=false'
-        },
+  it("should display instantiation form", () => {
+    const wrapper = shallow(<Instantiation {...props} />);
 
+    expect(wrapper.find("iframe").prop("src")).toEqual(
+      "../../../../process/My process name/1.0/content/?id=1&autoInstantiate=false"
+    );
+  });
+
+  it("should add an event listenenr when component is created", () => {
+    window.addEventListener = jest.fn();
+    const wrapper = shallow(<Instantiation {...props} />);
+
+    expect(window.addEventListener).toHaveBeenCalled();
+  });
+
+  it("should redirect to home page after submit", () => {
+    const testProps = {
+      ...{
+        history: { push: jest.fn() }
+      },
+      ...props
+    };
+    const wrapper = shallow(<Instantiation {...testProps} />);
+    let event = {
+      data: {
+        action: "Start process",
+        message: "success"
+      }
     };
 
-    it('should display instantiation form', () => {
-        const wrapper = shallow(<Instantiation {...props} />);
+    wrapper.instance().onFormSubmited(event);
 
-        expect(wrapper.find('iframe').prop('src')).toEqual(
-            '../../../../process/My process name/1.0/content/?id=1&autoInstantiate=false'
-        );
-    });
-
-    it('should redirect to home page after submit', () => {
-        global.document.addEventListener = jest.fn();
-        const wrapper = shallow(<Instantiation {...props} />);
-        expect(global.document.addEventListener).toHaveBeenCalled();
-
-
-       /* // Simulate a message being sent
-        var event = {
-            data:
-                {
-                    action: 'Start process',
-                    message: 'success'
-                }
-        };
-        window._listeners.message.false[0](event);
-
-        expect('window').toEqual('SomePseudoRandomAuthenticationKey');*/
-    });
+    expect(testProps.history.push).toHaveBeenCalledWith("/");
+  });
 });
