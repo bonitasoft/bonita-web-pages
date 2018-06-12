513f53cb9e8678a5c41c934c5942030e0bc8d89b
julienmege <julien.mege@bonitasoft.com>
2018-06-12 15:58:35 +0200

    add test on componentWillUnmount

--- page-process-list/src/pages/Main/components/Instantiation/Instanciation.test.js
+++ page-process-list/src/pages/Main/components/Instantiation/Instanciation.test.js
@@ -33,14 +33,61 @@ describe("Instantiation page", () => {
 
         // Simulate a message being sent
         var message = {
-                    action: 'Start process',
-                    message: 'success'
-                };
+            action: 'Start process',
+            message: 'success'
+        };
 
         window.postMessage(message, '*');
         //New to add this Promise to allows message to be read
         await new Promise(resolve => setTimeout(resolve, 0));
         expect(testProps.history.push).toHaveBeenCalledWith("/");
+    });
+
+    it('should not update history on error submit message.', async () => {
+        const testProps = {
+            ...props,
+            history: {push: jest.fn()}
+        };
+        const wrapper = shallow(<Instantiation {...testProps} />);
+
+        // Simulate a message being sent
+        var message = {
+            action: 'Start process',
+            message: 'error'
+        };
+
+        window.postMessage(message, '*');
+        //New to add this Promise to allows message to be read
+        await new Promise(resolve => setTimeout(resolve, 0));
+        expect(testProps.history.push).not.toHaveBeenCalled();
+    });
+
+    it('should not update history on success random message.', async () => {
+        const testProps = {
+            ...props,
+            history: {push: jest.fn()}
+        };
+        const wrapper = shallow(<Instantiation {...testProps} />);
+
+        // Simulate a message being sent
+        var message = {
+            action: 'Success random',
+            message: 'success'
+        };
+
+        window.postMessage(message, '*');
+        //New to add this Promise to allows message to be read
+        await new Promise(resolve => setTimeout(resolve, 0));
+        expect(testProps.history.push).not.toHaveBeenCalled();
+    });
+
+    it('should not update history on success random message.', async () => {
+        window.addEventListener = jest.fn(() => 'EventMock');
+        window.removeEventListener = jest.fn();
+
+        const wrapper = shallow(<Instantiation {...props} />);
+        wrapper.unmount();
+        expect(window.removeEventListener).toHaveBeenCalledWith('message', 'EventMock');
     });
 
 });
