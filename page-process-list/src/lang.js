d1b5b60c310c830d684c7cbff002a939da013168
benjaminparisel <benjamin.parisel@bonitasoft.com>
2018-07-20 16:24:44 +0200

    Update es translation

--- page-process-list/src/lang.js
+++ page-process-list/src/lang.js
@@ -1,74 +1,74 @@
 export default () => {
-  return {
-    en: {
-      translations: {
-        Filters: 'Filters',
-        Processes: 'Processes',
-        Search: '',
-        Name: 'Name',
-        Version: 'Version',
-        Categories: 'Categories',
-        Description: 'Description',
-        'Start a new case': 'Start a new case',
-        AllProcesses: 'All processes',
-        'No process to display': 'No process to display',
-        List: 'List',
-        of: 'of',
-        'The case {{caseId}} has been started successfully.':
-          'The case {{caseId}} has been started successfully.',
-        'Error while starting the case.': 'Error while starting the case.',
-        Start: 'Start',
-        Cancel: 'Cancel',
-        'Start a new case for process {{displayName}}':
-          'Start a new case for process {{displayName}}'
-      }
-    },
-    fr: {
-      translations: {
-        Filters: 'Filtres',
-        Processes: 'Processus',
-        Search: 'Rechercher',
-        Name: 'Nom',
-        Version: 'Version',
-        Categories: 'Catégories',
-        Description: 'Description',
-        'Start a new case': 'Démarrer un nouveau cas',
-        AllProcesses: 'Tous',
-        'No process to display': 'Aucun processus ne correspond à la recherche',
-        List: 'Liste',
-        of: 'de',
-        'The case {{caseId}} has been started successfully.':
-          'Le cas {{caseId}} a bien été démarré.',
-        'Error while starting the case.':
-          'Une erreur est survenue au démarrage du cas.',
-        Start: 'Démarrer',
-        Cancel: 'Annuler',
-        'Start a new case for process {{displayName}}':
-          'Démarrer un nouveau cas pour le processus {{displayName}}'
-      }
-    },
-    es: {
-      translations: {
-        Filters: 'Filtros',
-        Processes: 'Procesos',
-        Search: 'Bùsqueda',
-        Name: 'Nombre',
-        Version: 'Version',
-        Categories: 'Categorías',
-        Description: 'Descripción',
-        'Start a new case': 'Comience un nuevo caso',
-        AllProcesses: 'Todos',
-        'No process to display': 'Sin proceso para mostrar',
-        List: 'Lista',
-        of: 'de',
-        'The case {{caseId}} has been started successfully.':
-          'El caso {{caseId}} se ha iniciado con éxito.',
-        'Error while starting the case.': 'Error al iniciar el caso.',
-        Start: 'Inicio',
-        Cancel: 'Cancelar',
-        'Start a new case for process {{displayName}}':
-          'Comience un nuevo caso para el procesos {{displayName}}'
-      }
-    }
-  };
+    return {
+        en: {
+            translations: {
+                Filters: 'Filters',
+                Processes: 'Processes',
+                Search: '',
+                Name: 'Name',
+                Version: 'Version',
+                Categories: 'Categories',
+                Description: 'Description',
+                'Start a new case': 'Start a new case',
+                AllProcesses: 'All processes',
+                'No process to display': 'No process to display',
+                List: 'List',
+                of: 'of',
+                'The case {{caseId}} has been started successfully.':
+                    'The case {{caseId}} has been started successfully.',
+                'Error while starting the case.': 'Error while starting the case.',
+                Start: 'Start',
+                Cancel: 'Cancel',
+                'Start a new case for process {{displayName}}':
+                    'Start a new case for process {{displayName}}'
+            }
+        },
+        fr: {
+            translations: {
+                Filters: 'Filtres',
+                Processes: 'Processus',
+                Search: 'Rechercher',
+                Name: 'Nom',
+                Version: 'Version',
+                Categories: 'Catégories',
+                Description: 'Description',
+                'Start a new case': 'Démarrer un nouveau cas',
+                AllProcesses: 'Tous',
+                'No process to display': 'Aucun processus ne correspond à la recherche',
+                List: 'Liste',
+                of: 'de',
+                'The case {{caseId}} has been started successfully.':
+                    'Le cas {{caseId}} a bien été démarré.',
+                'Error while starting the case.':
+                    'Une erreur est survenue au démarrage du cas.',
+                Start: 'Démarrer',
+                Cancel: 'Annuler',
+                'Start a new case for process {{displayName}}':
+                    'Démarrer un nouveau cas pour le processus {{displayName}}'
+            }
+        },
+        es: {
+            translations: {
+                Filters: 'Filtros',
+                Processes: 'Procesos',
+                Search: 'Búsqueda',
+                Name: 'Nombre',
+                Version: 'Versión',
+                Categories: 'Categorías',
+                Description: 'Descripción',
+                'Start a new case': 'Comenzar un nuevo caso',
+                AllProcesses: 'Todos',
+                'No process to display': 'Sin proceso para mostrar',
+                List: 'Lista',
+                of: 'de',
+                'The case {{caseId}} has been started successfully.':
+                    'El caso {{caseId}} se ha iniciado con éxito.',
+                'Error while starting the case.': 'Error al iniciar el caso.',
+                Start: 'Inicio',
+                Cancel: 'Cancelar',
+                'Start a new case for process {{displayName}}':
+                    'Comenzar un nuevo caso para el proceso {{displayName}}'
+            }
+        }
+    };
 };
