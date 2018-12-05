2bc68e8442c36f951b5b29a6e01e62030a00b8d1
Dumitru Corini <dumitru.corini@bonitasoft.com>
2018-12-05 10:03:05 +0100

    init page

---
+++ uid-pages/page-import-export-organization/step_definitions/import-export.js
@@ -0,0 +1,30 @@
+const url = 'build/dist/resources/index.html';
+
+/*then(`I\'m user with {string} bos_local`, (text) => {
+  cy.setCookie('BOS_Locale', text);
+});
+
+when('I open tenant-status page', () => {
+  cy.visit(url);
+});
+
+given('Server tenant is running', () => {
+  cy.server();
+  cy.route('GET', 'build/dist/API/system/tenant/*', 'fixture:tenantRunning').as('tenant');
+});
+
+given('Server tenant is paused', () => {
+  cy.server();
+  cy.route('GET', 'build/dist/API/system/tenant/*', 'fixture:tenantPaused').as('tenant');
+});
+
+given('I\'m logged as technical user', () => {
+  cy.server();
+  cy.route('GET', 'build/dist/API/system/session/unusedId', 'fixture:technicalUser').as('session');
+});
+
+then(`I see {string} button`, (text) => {
+  cy.wait(['@tenant', '@session']);
+  cy.get('.ng-binding').should('have.text', text);
+});*/
+
---
+++ uid-pages/page-import-export-organization/test/specs/import-export-organization.feature
@@ -0,0 +1,8 @@
+Feature: The Import-Export Organization
+
+  #Scenario: Tenant is paused
+ #   Given I'm user with "en" bos_local
+ #   Given Server tenant is paused
+ #   Given I'm logged as technical user
+ #   When I open tenant-status page
+ #   Then I see "Resume" button
