248040dde5467b8b3f03e5e5a68b49904114881e
DMyTryC <dumitru.corini@gmail.com>
2019-03-12 10:30:45 +0100

    feat(bonita-portal-layout) Adding header to the bonita portal (#119)
    * Adding new header widget
    
    * Adding tests

--- uid-pages/layout-bonita/step_definitions/layoutBonita.js
+++ uid-pages/layout-bonita/step_definitions/layoutBonita.js
@@ -1,9 +1,13 @@
 const url = 'build/dist/resources/index.html';
 
-when('I open the Bonita layout with the {string} application selected', () => {
+given('The resolution is set to mobile', () => {
+    cy.viewport(767, 1000);
+});
+
+given('I have the {string} application selected', () => {
     cy.server();
-    cy.fixture('app1.json').as('app1');
-    cy.fixture('pageList.json').as('pageList');
+    cy.fixture('json/app1.json').as('app1');
+    cy.fixture('json/pageList.json').as('pageList');
     cy.route({
         method: 'GET',
         url: 'build/dist/API/living/application/*',
@@ -14,14 +18,127 @@ when('I open the Bonita layout with the {string} application selected', () => {
         url: 'build/dist/API/living/application-menu/**',
         response: '@pageList'
     }).as('pageListRoute');
+});
 
+given('A user is connected', () => {
+    cy.fixture('json/unusedId.json').as('unusedId');
+    cy.route({
+        method: 'GET',
+        url: '/build/dist/API/system/session/*',
+        response: '@unusedId'
+    }).as('unusedIdRoute');
+});
+
+given('The user has a first and last name defined', () => {
+    cy.fixture('json/userFull.json').as('userFull');
+    cy.route({
+        method: 'GET',
+        url: '/build/dist/API/identity/user/*',
+        response: '@userFull'
+    }).as('userFullRoute');
+});
+
+given('The user doesn\'t have a {string} info available', (unavailableInfo) => {
+    switch (unavailableInfo) {
+        case 'firstname' :
+            cy.fixture('json/userNoFirstname.json').as('userNoFirstname');
+            cy.route({
+                method: 'GET',
+                url: '/build/dist/API/identity/user/*',
+                response: '@userNoFirstname'
+            }).as('userNoFirstnameRoute');
+            break;
+        case 'lastname' :
+            cy.fixture('json/userNoLastname.json').as('userNoLastname');
+            cy.route({
+                method: 'GET',
+                url: '/build/dist/API/identity/user/*',
+                response: '@userNoLastname'
+            }).as('userNoLastnameRoute');
+            break;
+    }
+});
+
+when('I visit the index page', () => {
     cy.visit(url);
 });
 
+when('I click the burger', () => {
+    cy.get('li.ng-scope > .ng-scope').should('not.be.visible');
+    cy.get('.navbar-toggle').click();
+});
+
+when('I should not see the first line', () => {
+    cy.get('div.notShownInMobile').should('not.be.visible');
+});
+
 then( 'The application displayName is {string}', (appName) => {
+    cy.get('pb-link > .text-left > .ng-binding').should('have.text', appName);
+});
+
+then('The {string} page displayName is {string}', (pageNumber, pageName) => {
+    switch (pageNumber) {
+        case 'first' :
+            cy.get('li.ng-scope > .ng-scope').eq(0).should('have.text', pageName);
+            break;
+        case 'second' :
+            cy.get('li.ng-scope > .ng-scope').eq(1).should('have.text', pageName);
+            break;
+    }
+});
+
+then('I see {string} as the user name', (userName) => {
+    cy.get('.text-right > .ng-binding').should('have.text', userName);
+});
+
+then('I don\'t see {string} as the user name', (userName) => {
+    cy.get('.text-right > .ng-binding').should('not.have.text', userName);
+});
+
+then('I see the app selection icon', () => {
+    cy.get('.ng-binding > .glyphicon').should('have.attr', 'class', 'glyphicon glyphicon-th');
+});
+
+then('The image has the correct source', () => {
+    cy.get('img.img-responsive.ng-scope').should('have.attr','src', '../theme/images/logo.png');
+});
+
+then('The burger shows correctly', () => {
+    cy.get('.navbar-toggle > span').should((navbarToggleSpans) => {
+        expect(navbarToggleSpans).to.have.length(3);
+        for(var spanIndex = 0; spanIndex < navbarToggleSpans.length; spanIndex++) {
+            expect(navbarToggleSpans.eq(spanIndex)).to.have.class('icon-bar');
+        }
+    });
+});
+
+then('I see the page name dropdown', () => {
+    cy.get('li.ng-scope > .ng-scope').should('be.visible');
+});
+
+then('The {string} page displayName in dropdown is {string}', (pageNumber, pageName) => {
+    switch (pageNumber) {
+        case 'first' :
+            cy.get('.maxTitleWidth').eq(0).should('have.text', pageName);
+            break;
+        case 'second' :
+            cy.get('.maxTitleWidth').eq(1).should('have.text', pageName);
+            break;
+    }
+});
+
+then('The application displayName is {string} and is shown in the navbar', (appName) => {
     cy.get('.navbar-brand').should('have.text', appName);
 });
 
-then('The page displayName is {string}', (pageName) => {
-    cy.get('li.ng-scope > .ng-binding').should('have.text', pageName);
+then('I see {string} as the user name in the dropdown menu', (userName) => {
+    cy.get('.shownOnlyInMobile > li > a').eq(0).should('have.text', userName);
+});
+
+then('I don\'t see {string} as the user name in the dropdown menu', (userName) => {
+    cy.get('.shownOnlyInMobile > li > a').eq(0).should('not.have.text', userName);
+});
+
+then('I see the app selection icon in the dropdown menu', () => {
+    cy.get('a > .glyphicon').should('have.attr', 'class', 'glyphicon glyphicon-th');
 });
