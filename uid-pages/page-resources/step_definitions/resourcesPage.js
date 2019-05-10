f0c921a38af536bac20aa706c0b30d7a9872900b
VishalThapaliya <vishal.thapaliya@gmail.com>
2019-05-10 17:16:56 +0200

    Adding tests

--- uid-pages/page-resources/step_definitions/resourcesPage.js
+++ uid-pages/page-resources/step_definitions/resourcesPage.js
@@ -1,1 +1,19 @@
 const url = 'build/dist/resources/index.html';
+
+given('The user has resources', () => {
+    cy.server();
+    cy.fixture('json/resourceList.json').as('resourceList');
+    cy.route({
+        method: 'GET',
+        url: 'build/dist/API/portal/page?p=0&c=999',
+        response: '@resourceList'
+    });
+});
+
+when('The user visits the resources page', () => {
+    cy.visit(url);
+})
+
+then('The user see the list resources', () => {
+    cy.get('.app-item').should('have.length', 2);
+})
--- uid-pages/page-resources/test/specs/resourcesPageBonita.feature
+++ uid-pages/page-resources/test/specs/resourcesPageBonita.feature
@@ -1,1 +1,6 @@
 Feature: The resources page
+
+  Scenario: Resources are displayed correctly
+    Given The user has resources
+    When The user visits the resources page
+    Then The user see the list resources
