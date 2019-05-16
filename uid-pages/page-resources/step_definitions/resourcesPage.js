ae0b299a0ae89d68a8d699a3f14ae0eb3b5c173c
VishalThapaliya <vishal.thapaliya@gmail.com>
2019-05-16 10:45:18 +0200

    adding test for filter by type

--- uid-pages/page-resources/step_definitions/resourcesPage.js
+++ uid-pages/page-resources/step_definitions/resourcesPage.js
@@ -14,9 +14,18 @@ when('The user visits the resources page', () => {
     cy.visit(url);
 })
 
-then('The user see the list resources', () => {
+then('The user sees the list of resources', () => {
     cy.get('.app-item').should('have.length', 2);
 })
+
+given('The filter responses are defined for the type of resources', () => {
+    cy.fixture('json/filteredResourcesByPage.json').as('filteredResourcesByPage');
+    cy.route({
+        method: 'GET',
+        url: 'build/dist/API/portal/page?p=0&c=999&f=contentType=page*',
+        response: '@filteredResourcesByPage'
+    }).as('filteredResourcesByPageRoute');
+});
 
 then('The {string} resource displays {string} as title', (resource,title) => {
     switch (resource) {
@@ -37,4 +46,24 @@ then('The {string} resource displays {string} as description', (resource, descri
             cy.get('.itemDescription').eq(1).contains(description);
             break;
     }
-})
+});
+
+then('The user sees the filter dropdown', () => {
+    cy.get('pb-select .form-control').should('be.visible');
+});
+
+when('The user select {string} type', (type) => {
+    cy.get('.form-control').eq(0).select(type);
+});
+
+ then('The user sees only the list of page resources', () => {
+    cy.wait('@filteredResourcesByPageRoute');
+    cy.get('.itemName').should('have.length', 1);
+    cy.get('.itemName').eq(0).contains("API extension viewer page");
+ });
+
+ then('The user won\'t be able to see the list of other type of resources', () => {
+     cy.get('.itemName').contains('REST API extension example').should('not.exist');
+ });
+
+
--- uid-pages/page-resources/test/specs/resourcesPageBonita.feature
+++ uid-pages/page-resources/test/specs/resourcesPageBonita.feature
@@ -3,7 +3,7 @@ Feature: The resources page
   Scenario: Resources are displayed correctly
     Given The user has resources
     When The user visits the resources page
-    Then The user see the list resources
+    Then The user sees the list of resources
 
   Scenario: The resource in the list displays all the information correctly
     Given The user has resources
@@ -12,3 +12,15 @@ Feature: The resources page
     And The "first" resource displays "REST API extension example archive for Bonita Portal. Included examples: Get/Post, how to use a logger, customize response, call SOAP web service." as description
     And The "second" resource displays "API extension viewer page" as title
     And The "second" resource displays "API extension viewer page generated with Bonita UI designer" as description
+
+
+  Scenario: The resources page has filter by option
+    Given The user has resources
+    And The filter responses are defined for the type of resources
+    When The user visits the resources page
+    Then The user sees the list of resources
+    And The user sees the filter dropdown
+    When The user select "Page" type
+    Then The user sees only the list of page resources
+    And The user won't be able to see the list of other type of resources
+
