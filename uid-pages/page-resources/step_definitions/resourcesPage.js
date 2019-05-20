a03f1e94f4808e38b1b2202e374caeaad2cab709
VishalThapaliya <vishal.thapaliya@gmail.com>
2019-05-20 16:18:21 +0200

    responsive resources page without media queries

--- uid-pages/page-resources/step_definitions/resourcesPage.js
+++ uid-pages/page-resources/step_definitions/resourcesPage.js
@@ -15,7 +15,7 @@ when('The user visits the resources page', () => {
 })
 
 then('The user sees the list of resources', () => {
-    cy.get('.app-item').should('have.length', 2);
+    cy.get('.app-item').should('have.length', 3);
 })
 
 given('The filter responses are defined for the type of resources', () => {
@@ -35,6 +35,9 @@ then('The {string} resource displays {string} as title', (resource,title) => {
         case "second":
             cy.get('.itemName').eq(1).contains(title);
             break;
+        case "third":
+            cy.get('.itemName').eq(2).contains(title);
+            break;
     }
 })
 then('The {string} resource displays {string} as description', (resource, description) => {
@@ -45,21 +48,25 @@ then('The {string} resource displays {string} as description', (resource, descri
         case "second":
             cy.get('.itemDescription').eq(1).contains(description);
             break;
+        case "third":
+            cy.get('.itemDescription').eq(2).contains(description);
+            break;
     }
 });
 
 then('The user sees the filter dropdown', () => {
-    cy.get('pb-select .form-control').should('be.visible');
+    cy.get('pb-select select[name="pbSelect0"]').should('be.visible');
 });
 
 when('The user select {string} type', (type) => {
     cy.get('.form-control').eq(0).select(type);
 });
 
- then('The user sees only the list of page resources', () => {
+ then('The user sees only the list of {string} type resources', () => {
     cy.wait('@filteredResourcesByPageRoute');
-    cy.get('.itemName').should('have.length', 1);
+    cy.get('.itemName').should('have.length', 2);
     cy.get('.itemName').eq(0).contains("API extension viewer page");
+     cy.get('.itemName').eq(1).contains("TESTTESTTESTTESTTESTTESTTESTTESTTEST");
  });
 
  then('The user won\'t be able to see the list of other type of resources', () => {
@@ -68,5 +75,18 @@ when('The user select {string} type', (type) => {
 
 
 given('The sort responses are defined for the order of resources to be shown in the list', () => {
+    cy.fixture('json/sortedResourcesByUpdatedDateDESC.json').as('sortedResourcesByUpdatedDateDESC');
+    cy.route({
+        method: 'GET',
+        url: 'build/dist/API/portal/page?p=0&c=999&o=lastUpdateDate%20DESC*',
+        response: '@sortedResourcesByUpdatedDateDESC'
+    }).as('sortedResourcesByUpdatedDateDESCRoute');
+});
 
+then('The user sees the sort by dropdown', () => {
+    cy.get('select[name="pbSelect"]').should('be.visible');
+});
+
+when('The user select {string} option', (sorting) => {
+    cy.get('pb-select select[name="pbSelect1"]').eq(0).select(sorting);
 });
--- uid-pages/page-resources/test/specs/resourcesPageBonita.feature
+++ uid-pages/page-resources/test/specs/resourcesPageBonita.feature
@@ -12,23 +12,26 @@ Feature: The resources page
     And The "first" resource displays "REST API extension example archive for Bonita Portal. Included examples: Get/Post, how to use a logger, customize response, call SOAP web service." as description
     And The "second" resource displays "API extension viewer page" as title
     And The "second" resource displays "API extension viewer page generated with Bonita UI designer" as description
+    And The "third" resource displays "TESTTESTTESTTESTTESTTESTTESTTESTTEST" as title
+    And The "third" resource displays "Page generated with Bonita UI designer" as description
 
 
-  Scenario: The resources page has filter by option
+  Scenario: The resources page has filter option
     Given The user has resources
     And The filter responses are defined for the type of resources
     When The user visits the resources page
     Then The user sees the list of resources
     And The user sees the filter dropdown
     When The user select "Page" type
-    Then The user sees only the list of page resources
+    Then The user sees only the list of "Page" type resources
     And The user won't be able to see the list of other type of resources
 
   Scenario: The resources page has sort by option
     Given The user has resources
     And The sort responses are defined for the order of resources to be shown in the list
-    When The user visits the reources page
+    When The user visits the resources page
     Then The user sees the list of resources
     And The user sees the sort by dropdown
-    And The user select "Alphabetical order (A-Z)"
-    Then The user sees the list in alphabetical ascending order
+    When The user select "Last update - newest first" option.
+    Then The user sees the list of resources by last updated date in descending order
+
