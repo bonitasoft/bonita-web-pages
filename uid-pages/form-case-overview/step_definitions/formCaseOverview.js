859061fb72f1a1790a56e65932bc086f2f267251
Dumitru Corini <dumitru.corini@bonitasoft.com>
2019-07-25 11:14:37 +0200

    fix(case overview) Fixing BIZ Data table headers not being shown (#184)
    * fix(case overview) Fixing BIZ Data table headers not being shown
    
    Covers [BPO-190](https://bonitasoft.atlassian.net/browse/BPO-190)

--- uid-pages/form-case-overview/step_definitions/formCaseOverview.js
+++ uid-pages/form-case-overview/step_definitions/formCaseOverview.js
@@ -7,28 +7,28 @@ given('The archived case {string} server response is defined', (archivedCaseId) 
     cy.route({
         method: 'GET',
         url: 'build/dist/API/bpm/archivedCase?c=1&d=started_by&d=processDefinitionId&f=sourceObjectId=' + archivedCaseId + '&p=0',
-        response: '@archivedCase',
+        response: '@archivedCase'
     }).as('archivedCaseRoute');
 });
 
-given('The archived case {string} context server response is defined', (archivedCaseId) => {
+given('The archived case {string} empty context server response is defined', (archivedCaseId) => {
     cy.server();
-    cy.fixture('json/archivedCaseContext.json').as('archivedCaseContext');
+    cy.fixture('json/archivedCaseEmptyContext.json').as('archivedCaseEmptyContext');
     cy.route({
         method: 'GET',
         url: 'build/dist/API/bpm/archivedCase/' + archivedCaseId + '/context',
-        response: '@archivedCaseContext',
-    }).as('archivedCaseContextRoute');
+        response: '@archivedCaseEmptyContext'
+    }).as('archivedCaseEmptyContextRoute');
 });
 
-given('The archived case {string} document server response is defined', (archivedCaseId) => {
+given('The archived case {string} empty document server response is defined', (archivedCaseId) => {
     cy.server();
-    cy.fixture('json/archivedCaseDocument.json').as('archivedCaseDocument');
+    cy.fixture('json/archivedCaseEmptyDocument.json').as('archivedCaseEmptyDocument');
     cy.route({
         method: 'GET',
         url: 'build/dist/API/bpm/archivedCaseDocument?f=caseId=' + archivedCaseId,
-        response: '@archivedCaseDocument',
-    }).as('archivedCaseDocumentRoute');
+        response: '@archivedCaseEmptyDocument'
+    }).as('archivedCaseEmptyDocumentRoute');
 });
 
 given('The open case {string} server response is defined', (openCaseId) => {
@@ -37,10 +37,30 @@ given('The open case {string} server response is defined', (openCaseId) => {
     cy.route({
         method: 'GET',
         url: 'build/dist/API/bpm/case/' + openCaseId + '?d=started_by&d=processDefinitionId',
-        response: '@openCase',
+        response: '@openCase'
     }).as('openCaseRoute');
 });
 
+
+given('The open case {string} empty context server response is defined', (openCaseId) => {
+    cy.server();
+    cy.fixture('json/openCaseEmptyContext.json').as('openCaseEmptyContext');
+    cy.route({
+        method: 'GET',
+        url: 'build/dist/API/bpm/case/' + openCaseId + '/context',
+        response: '@openCaseEmptyContext'
+    }).as('openCaseEmptyContextRoute');
+});
+
+given('The open case {string} empty document server response is defined', (openCaseId) => {
+    cy.server();
+    cy.fixture('json/openCaseEmptyDocument.json').as('openCaseEmptyDocument');
+    cy.route({
+        method: 'GET',
+        url: 'build/dist/API/bpm/caseDocument?f=caseId=' + openCaseId,
+        response: '@openCaseEmptyDocument'
+    }).as('openCaseEmptyDocumentRoute');
+});
 
 given('The open case {string} context server response is defined', (openCaseId) => {
     cy.server();
@@ -48,18 +68,18 @@ given('The open case {string} context server response is defined', (openCaseId) 
     cy.route({
         method: 'GET',
         url: 'build/dist/API/bpm/case/' + openCaseId + '/context',
-        response: '@openCaseContext',
+        response: '@openCaseContext'
     }).as('openCaseContextRoute');
 });
 
-given('The open case {string} document server response is defined', (openCaseId) => {
+given('The open case business data is defined', () => {
     cy.server();
-    cy.fixture('json/openCaseDocument.json').as('openCaseDocument');
+    cy.fixture('json/businessData.json').as('businessData');
     cy.route({
         method: 'GET',
-        url: 'build/dist/API/bpm/caseDocument?f=caseId=' + openCaseId,
-        response: '@openCaseDocument',
-    }).as('openCaseDocumentRoute');
+        url: '/build/dist/API/bdm/businessData/com.company.model.VacationRequest/1',
+        response: '@businessData'
+    }).as('businessDataRoute');
 });
 
 when('I visit the archived case index page', () => {
@@ -71,16 +91,32 @@ when('I visit the open case index page', () => {
 });
 
 then('I can see both IDs have correct values', () => {
-    cy.wait('@archivedCaseContextRoute');
+    cy.wait('@archivedCaseEmptyContextRoute');
     cy.get('pb-title').contains('Case id').contains('10002');
     cy.get('pb-title').contains('Archived case id').contains('30003');
 });
 
 then('I can see the open case ID', () => {
-    cy.wait('@openCaseContextRoute');
+    cy.wait('@openCaseEmptyContextRoute');
     cy.get('pb-title').contains('Case id').contains('30004');
 });
 
 then('I cannot see the archived case ID', () => {
     cy.get('pb-title').contains('Archived case id').should('not.be.visible');
+});
+
+then('The correct BDM headers are visible', () => {
+    cy.get('th').should('have.length', '2');
+    cy.get('th').contains('requesterBonitaBPMId').should('be.visible');
+    cy.get('th').contains('name_string').should('be.visible');
+});
+
+then('The incorrect BDM headers don\'t exist', () => {
+    cy.get('th').contains('persistenceId').should('not.exist');
+    cy.get('th').contains('persistenceId_string').should('not.exist');
+    cy.get('th').contains('persistenceVersion').should('not.exist');
+    cy.get('th').contains('persistenceVersion_string').should('not.exist');
+    cy.get('th').contains('requesterBonitaBPMId_string').should('not.exist');
+    cy.get('th').contains('links_string').should('not.exist');
+    cy.get('th').contains('links').should('not.exist');
 });
