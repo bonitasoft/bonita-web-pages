9c7038a8d7522861e4891f863ed9ba6689eeb223
anthony <anthony.birembaut@bonitasoft.com>
2019-10-04 16:59:22 +0200

    feat(release pipeline) integrate web-pages
    add scripts to integrate web pages in release pipeline
    
    Covers [CI-467](https://bonitasoft.atlassian.net/browse/CI-467)

---
+++ infrastructure/create-tag.pipeline.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+def tag = params.TAG_NAME
+pipeline {
+    agent any
+    options {
+        timestamps()
+        skipDefaultCheckout true
+    }
+    stages {
+        stage('Init') {
+            steps{
+                script{
+                    git branch: params.BASE_BRANCH, url: 'git@github.com:bonitasoft/bonita-web-pages.git'
+                }
+            }
+
+        }
+        stage('Tag') {
+            steps {
+                script {
+                    sh "./infrastructure/release.sh ${tag} true"
+                }
+            }
+        }
+    }
+}
