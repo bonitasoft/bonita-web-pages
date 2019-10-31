878e07a4bf6e6b2dfda22d9b9a2e0c5f403af793
anthony <anthony.birembaut@bonitasoft.com>
2019-10-31 16:21:48 +0100

    feat(guest user) handle guest user in layout
    - handle guest user in menu widget (login button)
    - update menu widget version
    - restore style.css (move content from menu widget)
    - update layout page version
    
    Covers [BPO-283](https://bonitasoft.atlassian.net/browse/BPO-283)

--- widgets/customBonitaLayoutMenu/src/customLayoutMenuBonitaV3.ctrl.js
+++ widgets/customBonitaLayoutMenu/src/customLayoutMenuBonitaV3.ctrl.js
@@ -39,6 +39,13 @@ function WidgetlivingApplicationMenuController($scope, $http, $window, $location
     ctrl.openAppSelectionModal = function() {
         modalService.open($scope.properties.appSelectionModalId);
     };
+    
+    ctrl.logoutAndUpdateSuccessfulLogoutVariable = function() {
+        return $http.get($scope.properties.logoutURL)
+            .success(function(data) { 
+                $scope.properties.successfulLogoutResponse = data;
+            });
+    };
    
     //handle the browser back button
     $window.addEventListener('popstate', function(e) {
