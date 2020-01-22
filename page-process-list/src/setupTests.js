b41f005c990c2fd4aaad979f1260102e08c4889b
julienmege <julien.mege@bonitasoft.com>
2020-01-22 18:11:31 +0100

    feat(i18n) Use dedicated lang file for each language
    covers [BPO-4](https://bonitasoft.atlassian.net/browse/BPO-4)

--- page-process-list/src/setupTests.js
+++ page-process-list/src/setupTests.js
@@ -15,9 +15,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-jest.mock('i18next', () => ({
-  t: key => {
-    return key;
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  withTranslation: () => Component => {
+    Component.defaultProps = { ...Component.defaultProps, t: key => key };
+    return Component;
   }
 }));
 
