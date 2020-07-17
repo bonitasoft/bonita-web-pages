f1855787faa6278660c42d9079959b6e92dad09d
DumitruCorini <dumitru.corini@gmail.com>
2020-07-17 17:15:58 +0200

    feat (admin roles list) Add create role modal
    covers [BPO-572](https://bonitasoft.atlassian.net/browse/BPO-572)
    covers [BPO-573](https://bonitasoft.atlassian.net/browse/BPO-573)
    covers [BPO-574](https://bonitasoft.atlassian.net/browse/BPO-574)

--- uid-pages/page-admin-roles/step_definitions/adminRoles.js
+++ uid-pages/page-admin-roles/step_definitions/adminRoles.js
@@ -1,34 +1,59 @@
 const urlPrefix = 'build/dist/';
 const url = urlPrefix + 'resources/index.html';
-const rolesUrl = 'API/identity/role?';
+const rolesUrl = 'API/identity/role';
 const defaultFilters = '&o=displayName ASC';
-const defaultRequestUrl = urlPrefix + rolesUrl + 'c=20&p=0' + defaultFilters;
+const defaultRequestUrl = urlPrefix + rolesUrl + '?c=20&p=0' + defaultFilters;
+const refreshUrl = urlPrefix + rolesUrl + '?c=20&p=0' + defaultFilters + '&t=1*';
 
 given("The response {string} is defined", (responseType) => {
     cy.viewport(1366, 768);
     cy.server();
     switch (responseType) {
+        case 'refresh not called':
+            cy.route({
+                method: "GET",
+                url: refreshUrl,
+                onRequest: () => {
+                throw new Error("This should have not been called");
+                }
+            });
+            break;
         case 'default filter':
-            createRouteWithResponse(defaultRequestUrl, 'roles8Route', 'roles8');
+            createRouteWithResponse(defaultRequestUrl + '&t=0', 'roles8Route', 'roles8');
             break;
         case 'enable load more':
-            createRouteWithResponse(defaultRequestUrl,'roles20Route', 'roles20');
+            createRouteWithResponse(defaultRequestUrl + '&t=0','roles20Route', 'roles20');
             createRouteWithResponseAndPagination('', 'roles10Route', 'roles10', 2, 10);
             createRouteWithResponseAndPagination('', 'roles8Route', 'roles8', 3, 10);
             createRouteWithResponseAndPagination('', 'emptyResultRoute', 'emptyResult', 4, 10);
             break;
         case 'enable 20 load more':
-            createRouteWithResponse(defaultRequestUrl, 'roles20Route', 'roles20');
+            createRouteWithResponse(defaultRequestUrl + '&t=0', 'roles20Route', 'roles20');
             createRouteWithResponseAndPagination('', 'emptyResultRoute', 'emptyResult', 2, 10);
             break;
         case 'sort by':
-            createRoute(rolesUrl + 'c=20&p=0&o=displayName+DESC', 'sortDisplayNameDescRoute');
-            createRoute(rolesUrl + 'c=20&p=0&o=name+ASC', 'sortNameAscRoute');
-            createRoute(rolesUrl + 'c=20&p=0&o=name+DESC', 'sortNameDescRoute');
+            createRoute(rolesUrl + '?c=20&p=0&o=displayName+DESC&t=0', 'sortDisplayNameDescRoute');
+            createRoute(rolesUrl + '?c=20&p=0&o=name+ASC&t=0', 'sortNameAscRoute');
+            createRoute(rolesUrl + '?c=20&p=0&o=name+DESC&t=0', 'sortNameDescRoute');
             break;
         case 'search':
-            createRouteWithResponse(defaultRequestUrl + '&s=Member', 'searchMemberRoute', 'roles1');
-            createRouteWithResponse(defaultRequestUrl + '&s=Search term with no match', 'emptyResultRoute', 'emptyResult');
+            createRouteWithResponse(defaultRequestUrl + '&s=Member&t=0', 'searchMemberRoute', 'roles1');
+            createRouteWithResponse(defaultRequestUrl + '&s=Search term with no match&t=0 ', 'emptyResultRoute', 'emptyResult');
+            break;
+        case 'role creation success':
+            createPostRoute(rolesUrl, 'roleCreationRoute');
+            break;
+        case 'refresh list':
+            createRouteWithResponse(refreshUrl, 'refreshUrlRoute', 'roles9');
+            break;
+        case 'already exists':
+            createRouteWithResponseAndMethodAndStatus(urlPrefix + rolesUrl, 'createRoleAlreadyExistsRoute', 'createRoleAlreadyExists', 'POST', '403');
+            break;
+        case '403':
+            createPostRouteWithStatus(rolesUrl, 'unauthorizedCreateRoleRoute', '403');
+            break;
+        case '500':
+            createPostRouteWithStatus(rolesUrl, 'unauthorizedCreateRoleRoute', '500');
             break;
         default:
             throw new Error("Unsupported case");
@@ -37,42 +62,43 @@ given("The response {string} is defined", (responseType) => {
     function createRoute(urlSuffix, routeName) {
         cy.route({
             method: 'GET',
-            url: urlPrefix + urlSuffix,
+            url: urlPrefix + urlSuffix
         }).as(routeName);
     }
 
     function createPostRoute(urlSuffix, routeName) {
+        createPostRouteWithStatus(urlSuffix, routeName, 200);
+    }
+
+    function createRouteWithResponse(url, routeName, response) {
+        createRouteWithResponseAndMethod(url, routeName, response, 'GET');
+    }
+
+    function createRouteWithResponseAndMethod(url, routeName, response, method) {
+        createRouteWithResponseAndMethodAndStatus(url, routeName, response, method, 200);
+    }
+
+    function createPostRouteWithStatus(urlSuffix, routeName, status) {
         cy.route({
             method: 'POST',
             url: urlPrefix + urlSuffix,
-            response: ""
+            response: "",
+            status: status
         }).as(routeName);
     }
 
-    function createRouteWithResponse(urlSuffix, routeName, response) {
-        createRouteWithResponseAndMethod(urlSuffix, routeName, response, 'GET');
-    }
-
-    function createRouteWithResponseAndMethod(urlSuffix, routeName, response, method) {
+    function createRouteWithResponseAndMethodAndStatus(url, routeName, response, method, status) {
         cy.fixture('json/' + response + '.json').as(response);
         cy.route({
             method: method,
-            url: urlSuffix,
+            url: url,
+            status: status,
             response: '@' + response
         }).as(routeName);
     }
 
-    function createRouteWithMethodAndStatus(urlSuffix, routeName, method, status) {
-        cy.route({
-            method: method,
-            url: urlSuffix,
-            status: status,
-            response: ''
-        }).as(routeName);
-    }
-
     function createRouteWithResponseAndPagination(queryParameter, routeName, response, page, count) {
-        const loadMoreUrl = urlPrefix + rolesUrl + 'c=' + count + '&p=' + page + defaultFilters;
+        const loadMoreUrl = urlPrefix + rolesUrl + '?c=' + count + '&p=' + page + defaultFilters;
         let responseValue = undefined;
         if (response) {
             cy.fixture('json/' + response + '.json').as(response);
@@ -132,6 +158,32 @@ when("I put {string} in {string} filter field", (filterValue, filterType) => {
     }
 });
 
+when("I fill in the information", () => {
+    cy.get('.modal-body input').eq(0).type('Role name');
+    cy.get('.modal-body input').eq(1).type('Role display name');
+    cy.get('.modal-body input').eq(2).type('Role description');
+});
+
+when("I erase the search filter", () => {
+    cy.get('pb-input input:visible').clear();
+});
+
+when("I click on create button", () => {
+cy.contains('button', 'Create').click();
+});
+
+when("I click on the {string} button in modal", (buttonName) => {
+    cy.contains('.modal button', buttonName).click();
+});
+
+when("I fill in the name", () => {
+    cy.get('.modal-body input').eq(0).type('Role name');
+});
+
+when("I clear the name", () => {
+    cy.get('.modal-body input').eq(0).clear();
+});
+
 then("The roles page have the correct information", () => {
     cy.contains('h3', 'Roles');
     cy.get('.role-item').should('have.length', 8);
@@ -146,10 +198,6 @@ then("The roles page have the correct information", () => {
     cy.get('.role-item').eq(0).contains('.item-label', 'This is a description.');
     cy.get('.role-item').eq(0).get('.btn.btn-link .glyphicon-pencil');
     cy.get('.role-item').eq(0).get('.btn.btn-link .glyphicon-trash');
-});
-
-when("I erase the search filter", () => {
-    cy.get('pb-input input:visible').clear();
 });
 
 then("A list of {int} roles is displayed", (nbrOfItems) => {
@@ -185,4 +233,68 @@ then("The api call is made for {string}", (filterValue) => {
 then("No roles are available", () => {
     cy.get('.task-item:visible').should('have.length', 0);
     cy.contains('No roles to display').should('be.visible');
+});
+
+then("The create modal is open and has a default state for {string}", (state) => {
+    cy.contains('.modal-header h3', state).should('be.visible');
+    cy.get('.modal-body input').should('have.length', 3);
+    cy.contains('.modal-body', 'Name').should('be.visible');
+    cy.contains('.modal-body', 'Display name').should('be.visible');
+    cy.contains('.modal-body', 'Description').should('be.visible');
+    cy.get('.modal-body .glyphicon-remove-sign').should('not.be.visible');
+    cy.get('.modal-body .glyphicon-ok-sign').should('not.be.visible');
+    cy.contains('.modal-footer button', 'Create').should('be.disabled');
+    cy.contains('.modal-footer button', 'Cancel').should('be.visible');
+    cy.contains('.modal-footer button', 'Close').should('not.exist');
+});
+
+then("There is no modal displayed", () => {
+    cy.get('.modal').should('not.visible');
+});
+
+then("The creation is successful", () => {
+    cy.contains('.modal-footer button', 'Create').should('be.disabled');
+    cy.contains('.modal-footer button', 'Cancel').should('not.exist');
+    cy.contains('.modal-footer button', 'Close').should('be.visible');
+    cy.wait('@roleCreationRoute').then((xhr) => {
+        expect(xhr.request.body.name).to.equal('Role name');
+        expect(xhr.request.body.displayName).to.equal('Role display name');
+        expect(xhr.request.body.description).to.equal('Role description');
+    });
+    cy.get('.modal-body input').each((input) => {
+        expect(input).to.have.attr('readonly', 'readonly');
+    });
+});
+
+then("The roles list is refreshed", () => {
+    cy.wait('@refreshUrlRoute');
+});
+
+then("There is a error message about name being required", () => {
+    cy.contains('This field is required').should('be.visible');
+});
+
+then("The create button in modal is enabled", () => {
+    cy.contains('.modal-footer button', 'Create').should('be.enabled');
+});
+
+then("The create button in modal is disabled", () => {
+    cy.contains('.modal-footer button', 'Create').should('be.disabled');
+});
+
+then("I see {string} error message", (error) => {
+    switch (error) {
+        case '500':
+            cy.contains('.modal', 'An error has occurred. For more information, check the log file.').should('be.visible');
+            break;
+        case '403':
+            cy.contains('.modal', 'Access denied. For more information, check the log file.').should('be.visible');
+            break;
+        case 'already exists':
+            cy.contains('.modal', 'A role with the same name already exists.').should('be.visible');
+            break;
+        default:
+            throw new Error("Unsupported case");
+    }
+    cy.get('.modal').contains('The role has not been created.').should('be.visible');
 });
--- uid-pages/page-admin-roles/test/specs/adminRoles.feature
+++ uid-pages/page-admin-roles/test/specs/adminRoles.feature
@@ -52,3 +52,92 @@ Feature: The Admin Roles in desktop resolution
     Then A list of 8 roles is displayed
     When I put "Search term with no match" in "search" filter field
     Then No roles are available
+
+  Scenario: The create role modal is opened and closed
+    Given The response "refresh not called" is defined
+    And The response "default filter" is defined
+    When I visit the admin roles page
+    Then A list of 8 roles is displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
+    When I click on the "Cancel" button in modal
+    Then There is no modal displayed
+
+  Scenario: The create role modal creates successfully
+    Given The response "role creation success" is defined
+    And The response "refresh list" is defined
+    And The response "default filter" is defined
+    When I visit the admin roles page
+    Then A list of 8 roles is displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
+    When I fill in the information
+    Then The create button in modal is enabled
+    When I click on the "Create" button in modal
+    Then The creation is successful
+    And The roles list is refreshed
+    When I click on the "Close" button in modal
+    Then There is no modal displayed
+    And A list of 9 roles is displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
+
+  Scenario: The create role modal requires name
+    Given The response "refresh not called" is defined
+    And The response "default filter" is defined
+    When I visit the admin roles page
+    Then A list of 8 roles is displayed
+    When I click on create button
+    And I fill in the name
+    And I clear the name
+    Then There is a error message about name being required
+    And The create button in modal is disabled
+    When I click on the "Cancel" button in modal
+    Then There is no modal displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
+
+  Scenario: The modal should display already exists error message
+    Given The response "default filter" is defined
+    And The response "already exists" is defined
+    And The response "refresh not called" is defined
+    When I visit the admin roles page
+    Then A list of 8 roles is displayed
+    When I click on create button
+    And I fill in the information
+    And I click on the "Create" button in modal
+    Then I see "already exists" error message
+    When I click on the "Cancel" button in modal
+    Then There is no modal displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
+
+  Scenario: The modal should display generic 403 error message
+    Given The response "default filter" is defined
+    And The response "403" is defined
+    And The response "refresh not called" is defined
+    When I visit the admin roles page
+    Then A list of 8 roles is displayed
+    When I click on create button
+    And I fill in the information
+    And I click on the "Create" button in modal
+    Then I see "403" error message
+    When I click on the "Cancel" button in modal
+    Then There is no modal displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
+
+  Scenario: The modal should display generic 500 error message
+    Given The response "default filter" is defined
+    And The response "500" is defined
+    And The response "refresh not called" is defined
+    When I visit the admin roles page
+    Then A list of 8 roles is displayed
+    When I click on create button
+    And I fill in the information
+    And I click on the "Create" button in modal
+    Then I see "500" error message
+    When I click on the "Cancel" button in modal
+    Then There is no modal displayed
+    When I click on create button
+    Then The create modal is open and has a default state for "Create a role"
