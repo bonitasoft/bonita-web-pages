33dadb311a949e6c21dddf6ca34230dfc6a4e3ad
VishalThapaliya <vishal.thapaliya@gmail.com>
2020-08-21 18:08:13 +0200

    feat (admin profiles list) implement delete custom profile modal
    Covers [BPO-625] (https://bonitasoft.atlassian.net/browse/BPO-625)

--- uid-pages/page-admin-profiles/step_definitions/adminProfiles.js
+++ uid-pages/page-admin-profiles/step_definitions/adminProfiles.js
@@ -2,6 +2,7 @@ const urlPrefix = 'build/dist/';
 const profilesUrl = 'API/portal/profile';
 const defaultFilters = '&o=name ASC';
 const defaultRequestUrl = urlPrefix + profilesUrl + '?c=20&p=0' + defaultFilters;
+const refreshUrl = urlPrefix + profilesUrl + '?c=20&p=0' + defaultFilters + '&t=1*';
 
 given("The response {string} is defined", (responseType) => {
     cy.viewport(1366, 768);
@@ -17,34 +18,49 @@ given("The response {string} is defined", (responseType) => {
             });
             break;
         case 'default filter':
-            createRouteWithResponse(defaultRequestUrl, 'profiles8Route', 'profiles8');
+            createRouteWithResponse(defaultRequestUrl + '&t=0', 'profiles8Route', 'profiles8');
             break;
         case 'sort by':
-            createRoute(profilesUrl + '?c=20&p=0&o=name+ASC', 'sortNameAscRoute');
-            createRoute(profilesUrl + '?c=20&p=0&o=name+DESC', 'sortNameDescRoute');
+            createRoute(profilesUrl + '?c=20&p=0&o=name+ASC&t=0', 'sortNameAscRoute');
+            createRoute(profilesUrl + '?c=20&p=0&o=name+DESC&t=0', 'sortNameDescRoute');
             break;
         case 'search':
-            createRouteWithResponse(defaultRequestUrl + '&s=Administrator', 'searchAdministratorRoute', 'profiles1');
+            createRouteWithResponse(defaultRequestUrl + '&s=Administrator&t=0', 'searchAdministratorRoute', 'profiles1');
             createRouteWithResponse(defaultRequestUrl + '&s=Search term with no match', 'emptyResultRoute', 'emptyResult');
             break;
         case 'profiles load more':
-            createRouteWithResponse(defaultRequestUrl,'profiles20Route', 'profiles20');
+            createRouteWithResponse(defaultRequestUrl + '&t=0','profiles20Route', 'profiles20');
             createProfilesRouteWithResponseAndPagination('', 'profiles10Route', 'profiles10', 2, 10);
             createProfilesRouteWithResponseAndPagination('', 'profiles8Route', 'profiles8', 3, 10);
             createProfilesRouteWithResponseAndPagination('', 'emptyResultRoute', 'emptyResult', 4, 10);
             break;
         case 'profiles 20 load more':
-            createRouteWithResponse(defaultRequestUrl, 'profiles20Route', 'profiles20');
+            createRouteWithResponse(defaultRequestUrl + '&t=0', 'profiles20Route', 'profiles20');
             createProfilesRouteWithResponseAndPagination('', 'emptyResultRoute', 'emptyResult', 2, 10);
             break;
         case 'profiles 30 load more':
-            createRouteWithResponse(defaultRequestUrl, 'profiles20Route', 'profiles20');
+            createRouteWithResponse(defaultRequestUrl + '&t=0', 'profiles20Route', 'profiles20');
             createProfilesRouteWithResponseAndPagination('', 'profiles10Route', 'profiles10', 2, 10);
             createProfilesRouteWithResponseAndPagination('', 'emptyResultRoute', 'emptyResult', 3, 10);
             break;
         case 'sort during limitation':
-            createRouteWithResponse(urlPrefix + profilesUrl + '?c=20&p=0&o=name+DESC', 'sortDisplayNameDescRoute', 'profiles20');
+            createRouteWithResponse(urlPrefix + profilesUrl + '?c=20&p=0&o=name+DESC&t=0', 'sortDisplayNameDescRoute', 'profiles20');
             createRouteWithResponse(urlPrefix + profilesUrl + '?c=10&p=2&o=name+DESC', 'sortDisplayNameDescRoute2', 'profiles10');
+            break;
+        case 'profile deletion success':
+            createRouteWithMethod(profilesUrl + '/101', 'profileDeletionRoute', 'DELETE');
+            break;
+        case 'refresh list after delete':
+            createRouteWithResponse(refreshUrl, 'refreshUrlRoute', 'profiles7');
+            break;
+        case '403 during deletion':
+            createRouteWithMethodAndStatus(profilesUrl + '/101', 'unauthorizedDeleteProfileRoute', 'DELETE', '403');
+            break;
+        case '404 during deletion':
+            createRouteWithMethodAndStatus(profilesUrl + '/101', 'notFoundDeleteProfileRoute', 'DELETE', '404');
+            break;
+        case '500 during deletion':
+            createRouteWithMethodAndStatus(profilesUrl + '/101', 'internalErrorDeleteProfileRoute', 'DELETE', '500');
             break;
         default:
             throw new Error("Unsupported case");
@@ -54,6 +70,19 @@ given("The response {string} is defined", (responseType) => {
         cy.route({
             method: 'GET',
             url: urlPrefix + urlSuffix
+        }).as(routeName);
+    }
+
+    function createRouteWithMethod(urlSuffix, routeName, method) {
+        createRouteWithMethodAndStatus(urlSuffix, routeName, method, 200);
+    }
+
+    function createRouteWithMethodAndStatus(urlSuffix, routeName, method, status) {
+        cy.route({
+            method: method,
+            url: urlPrefix + urlSuffix,
+            response: "",
+            status: status
         }).as(routeName);
     }
 
@@ -134,6 +163,14 @@ when("I click on Load more profiles button", () => {
     cy.get('button').contains('Load more profiles').click();
 });
 
+when("I click on delete button for first profile", () => {
+    cy.get('.glyphicon.glyphicon-trash').eq(0).parent().click();
+});
+
+when("I click on the {string} button in modal footer", (buttonName) => {
+    cy.contains('.modal-footer button', buttonName).click();
+});
+
 then("The profiles page has the correct information", () => {
     cy.contains('h3', 'Profiles');
     cy.get('.profile-item').should('have.length', 8);
@@ -186,4 +223,52 @@ then("No profiles are displayed", () => {
 
 then("The load more profiles button is disabled", () => {
     cy.get('button').contains('Load more profiles').should('be.disabled');
+});
+
+then("The delete modal is open and has a default state for {string}", (state) => {
+    cy.contains('.modal-header h3', state).should('be.visible');
+    cy.contains('.modal-body p', 'Are you sure you want to delete this profile?');
+    cy.get('.modal-body .glyphicon-remove-sign').should('not.be.visible');
+    cy.get('.modal-body .glyphicon-ok-sign').should('not.be.visible');
+    cy.contains('.modal-footer button', 'Delete').should('be.enabled');
+    cy.contains('.modal-footer button', 'Cancel').should('be.visible');
+    cy.contains('.modal-footer button', 'Close').should('not.exist');
+});
+
+then("There is no modal displayed", () => {
+    cy.get('.modal').should('not.visible');
+});
+
+then("The deletion is successful", () => {
+    cy.contains('.modal-footer button', 'Delete').should('be.disabled');
+    cy.contains('.modal-footer button', 'Cancel').should('not.exist');
+    cy.contains('.modal-footer button', 'Close').should('be.visible');
+});
+
+then("The profiles list is refreshed", () => {
+    cy.wait('@refreshUrlRoute');
+});
+
+then("A list of {int} profiles is displayed", (nbrOfItems) => {
+    cy.get('.profile-item:visible').should('have.length', nbrOfItems);
+});
+
+then("I see {string} error message for {string}", (error, action) => {
+    switch (error) {
+        case 'already exists':
+            cy.contains('.modal-body', 'A profile with the same name already exists.').should('be.visible');
+            break;
+        case '403':
+            cy.contains('.modal-body', 'Access denied. For more information, check the log file.').should('be.visible');
+            break;
+        case '404':
+            cy.contains('.modal-body', 'The profile does not exist. Reload the page to see the new list of profiles.').should('be.visible');
+            break;
+        case '500':
+            cy.contains('.modal-body', 'An error has occurred. For more information, check the log file.').should('be.visible');
+            break;
+        default:
+            throw new Error("Unsupported case");
+    }
+    cy.get('.modal').contains('The profile has not been ' + action + '.').should('be.visible');
 });
