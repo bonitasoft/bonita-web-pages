60ecf56fb346d547b36126382314c7e777cb7a59
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2021-03-10 09:42:54 +0100

    Try to parallelize using gradle and new node plugin (#26)
    * chore(web pages): Parallelize using gradle and new node plugin
    
    covers [RUNTIME-168](https://bonitasoft.atlassian.net/browse/RUNTIME-168)

--- buildSrc/src/main/groovy/BonitaHtmlPagePlugin.groovy
+++ buildSrc/src/main/groovy/BonitaHtmlPagePlugin.groovy
@@ -6,22 +6,14 @@ class BonitaHtmlPagePlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def extension = project.extensions.create('bonitaPage', BonitaPagePluginExtension)
-        project.plugins.apply('com.moowork.node')
+        project.plugins.apply('com.github.node-gradle.node')
         project.plugins.apply('distribution')
-        def currentDir = project.rootProject.projectDir
-
-        project.beforeEvaluate {
-            project.node {
-                version = extension.nodeVersion
-                npmVersion = extension.npmVersion
-            }
-        }
+        def projectRootDir = project.rootProject.projectDir
 
         project.node {
             download = true
-
-            workDir = project.file("${currentDir}/.gradle/nodejs")
-            npmWorkDir = project.file("${currentDir}/.gradle/npm")
+            version = Versions.nodeVersion
+            npmVersion = Versions.npmVersion
         }
 
         project.tasks.npm_install.configure {
@@ -30,14 +22,14 @@ class BonitaHtmlPagePlugin implements Plugin<Project> {
             inputs.files('package.json')
         }
 
-        def buildPage = project.task([type: com.moowork.gradle.node.npm.NpmTask, dependsOn: project.tasks.npm_install], 'buildPage') {
+        def buildPage = project.task([type: com.github.gradle.node.npm.task.NpmTask, dependsOn: project.tasks.npm_install], 'buildPage') {
             group 'Bonita'
             description 'Build a ZIP which contains an custom-page to be imported in living application'
             args = ['run']
             inputs.files('package.json')
             inputs.dir('src')
             inputs.dir('resources')
-            outputs.dirs({extension.frontendBuildDir})
+            outputs.dirs({ extension.frontendBuildDir })
         }
 
         project.tasks.distZip.dependsOn buildPage
