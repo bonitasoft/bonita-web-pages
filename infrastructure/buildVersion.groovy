a6aebbb00ed1165a25d0e727844935b815dec166
Baptiste Mesta <baptiste.mesta@gmail.com>
2021-04-30 08:46:30 +0200

    chore(build): fix java 11 build version

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -3,12 +3,14 @@ pipeline {
     options {
         timestamps()
     }
+    environment {
+        JAVA_HOME = "${env.JAVA_HOME_11}"
+        PATH = "${env.JAVA_HOME_11}/bin:${env.PATH}"
+    }
     stages {
-        withEnv(["PATH=${env.JAVA_HOME_11}/bin:${env.PATH}", "JAVA_HOME=${env.JAVA_HOME_11}"]) {
-            stage('Build and deploy') {
-                steps {
-                    sh("./gradlew clean build publish -x test -PaltDeploymentRepository=${ALT_DEPLOYMENT_REPOSITORY_TAG}")
-                }
+        stage('Build and deploy') {
+            steps {
+                sh("./gradlew clean build publish -x test -PaltDeploymentRepository=${ALT_DEPLOYMENT_REPOSITORY_TAG}")
             }
         }
     }
