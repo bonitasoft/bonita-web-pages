dc6bfe3bd2d8d6529772e52d0c06813f90d4aec9
Baptiste Mesta <baptiste.mesta@gmail.com>
2021-04-30 08:23:23 +0200

    chore(build): set java 11 for tag scripts

--- infrastructure/createTag.groovy
+++ infrastructure/createTag.groovy
@@ -6,14 +6,16 @@ node {
     stage('Tag') {
         tag = params.TAG_NAME
         branch = params.BASE_BRANCH
-        withCredentials([usernamePassword(
-                credentialsId: 'github',
-                passwordVariable: 'GIT_PASSWORD',
-                usernameVariable: 'GIT_USERNAME')]) {
-            sh "git branch --force $branch origin/$branch"
-            sh "git checkout $branch"
+        withEnv(["PATH=${env.JAVA_HOME_11}/bin:${env.PATH}", "JAVA_HOME=${env.JAVA_HOME_11}"]) {
+            withCredentials([usernamePassword(
+                    credentialsId: 'github',
+                    passwordVariable: 'GIT_PASSWORD',
+                    usernameVariable: 'GIT_USERNAME')]) {
+                sh "git branch --force $branch origin/$branch"
+                sh "git checkout $branch"
 
-            sh "./gradlew release -Prelease.customUsername=${GIT_USERNAME} -Prelease.customPassword=${GIT_PASSWORD} -Prelease.version=$tag"
+                sh "./gradlew release -Prelease.customUsername=${GIT_USERNAME} -Prelease.customPassword=${GIT_PASSWORD} -Prelease.version=$tag"
+            }
         }
     }
 }
--- infrastructure/updateVersion.groovy
+++ infrastructure/updateVersion.groovy
@@ -5,14 +5,16 @@ node {
 
     stage('Update version') {
         branch = params.BASE_BRANCH
-        withCredentials([usernamePassword(
-                credentialsId: 'github',
-                passwordVariable: 'GIT_PASSWORD',
-                usernameVariable: 'GIT_USERNAME')]) {
-            sh "git branch --force $branch origin/$branch"
-            sh "git checkout $branch"
+        withEnv(["PATH=${env.JAVA_HOME_11}/bin:${env.PATH}", "JAVA_HOME=${env.JAVA_HOME_11}"]) {
+            withCredentials([usernamePassword(
+                    credentialsId: 'github',
+                    passwordVariable: 'GIT_PASSWORD',
+                    usernameVariable: 'GIT_USERNAME')]) {
+                sh "git branch --force $branch origin/$branch"
+                sh "git checkout $branch"
 
-            sh "./gradlew markNextVersion -Pnext.snapshot=${params.newVersion} -Prelease.customUsername=${GIT_USERNAME} -Prelease.customPassword=${GIT_PASSWORD}"
+                sh "./gradlew markNextVersion -Pnext.snapshot=${params.newVersion} -Prelease.customUsername=${GIT_USERNAME} -Prelease.customPassword=${GIT_PASSWORD}"
+            }
         }
     }
 }
