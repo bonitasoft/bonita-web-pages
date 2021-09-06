f18af6321c80a688e246bb48e2e966344d5d5bb8
Dumitru Corini <dumitru.corini@bonitasoft.com>
2021-09-06 16:12:16 +0200

    chore(release): Add push tag to community (#88)
    * chore(release): Add push tag to community

--- infrastructure/createTag.groovy
+++ infrastructure/createTag.groovy
@@ -6,6 +6,7 @@ node {
     stage('Tag') {
         tag = params.TAG_NAME
         branch = params.BASE_BRANCH
+        pushTagToCommunity = params.pushTagToCommunity
         withEnv(["PATH=${env.JAVA_HOME_11}/bin:${env.PATH}", "JAVA_HOME=${env.JAVA_HOME_11}"]) {
             withCredentials([usernamePassword(
                     credentialsId: 'github',
@@ -15,6 +16,10 @@ node {
                 sh "git checkout $branch"
 
                 sh "./gradlew release -Prelease.customUsername=${GIT_USERNAME} -Prelease.customPassword=${GIT_PASSWORD} -Prelease.version=$tag"
+                if (pushTagToCommunity) {
+                    sh "git remote add remote.origin.community.url https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/bonitasoft/bonita-web-pages.git"
+                    sh "git push remote.origin.community.url $tag"
+                }
             }
         }
     }
