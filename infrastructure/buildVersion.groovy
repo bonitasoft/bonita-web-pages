f8289bb7028339dad10a060a53a3eec28c782657
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2022-04-12 14:18:15 +0200

    fix(script): fix buildVersion script

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -7,7 +7,7 @@ pipeline {
         stage('Build and deploy') {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'jfrog', passwordVariable: 'REPOSITORY_PASSWORD', usernameVariable: 'REPOSITORY_USERNAME')]) {
-                    args = "build publish -x test"
+                    def args = "build publish -x test"
                     args += " -PextraRepositories=${env.ALT_DEPLOYMENT_REPOSITORY_SNAPSHOTS},${env.ALT_DEPLOYMENT_REPOSITORY_RELEASES}"
                     args += " -PreleasesUsername=${env.REPOSITORY_USERNAME}"
                     args += " -PreleasesPassword=${env.REPOSITORY_PASSWORD}"
